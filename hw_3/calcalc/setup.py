8614720bd0c05e679f8bf5f0777e43e9acbd1c17
Leotrinos <yanglyu2@berkeley.edu>
2022-02-12 14:03:01 -0800

    let setup.py read README.md

--- hw_3/calcalc/setup.py
+++ hw_3/calcalc/setup.py
@@ -1,12 +1,14 @@
-from setuptools import setup, find_packages
+from setuptools import setup
+from pathlib import Path
 
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(name='calcalc-toy',
       version='1.5',
       description='A calculator that evaluates string expressions ' +
                   'both locally or using WolframAlpha.',
-      long_description='A calculator that evaluates string numerical expressions ' +
-                       'both locally or using WolframAlpha.',
+      long_description=long_description,
       long_description_content_type="text/markdown",
       author='Yang Lyu',
       author_email='yanglyu902@gmail.com',
